ade77839fea238d47b51c95bc241ee6ac6a4b020
pvyarya <pranav.arya.pa@outlook.com>
2025-09-16 12:08:56 +0000

    test

--- README.md
+++ README.md
@@ -79,4 +79,3 @@ erDiagram
   employee ||--o{ employee_events : "employee_id"
   notes }o--o{ employee_events : ""
 ```
-#jghj
--- python-package/employee_events/team.py
+++ python-package/employee_events/team.py
@@ -1,46 +1,50 @@
 # Import the QueryBase class
-# YOUR CODE HERE
+from employee_events.query_base import QueryBase
 
 # Import dependencies for sql execution
-#### YOUR CODE HERE
+from .sql_execution import execute_sql, execute_sql_df
 
 # Create a subclass of QueryBase
-# called  `Team`
-#### YOUR CODE HERE
+# called `Team`
+class Team(QueryBase):
 
     # Set the class attribute `name`
     # to the string "team"
-    #### YOUR CODE HERE
-
+    name = "team"
 
     # Define a `names` method
     # that receives no arguments
     # This method should return
     # a list of tuples from an sql execution
-    #### YOUR CODE HERE
-        
+    def names(self):
         # Query 5
         # Write an SQL query that selects
         # the team_name and team_id columns
         # from the team table for all teams
         # in the database
-        #### YOUR CODE HERE
-    
+        query = """
+            SELECT team_name, team_id
+            FROM team
+        """
+        return execute_sql(query)
 
     # Define a `username` method
     # that receives an ID argument
     # This method should return
     # a list of tuples from an sql execution
-    #### YOUR CODE HERE
-
+    def username(self, id):
         # Query 6
         # Write an SQL query
         # that selects the team_name column
         # Use f-string formatting and a WHERE filter
         # to only return the team name related to
         # the ID argument
-        #### YOUR CODE HERE
-
+        query = f"""
+            SELECT team_name
+            FROM team
+            WHERE team_id = {id}
+        """
+        return execute_sql(query)
 
     # Below is method with an SQL query
     # This SQL query generates the data needed for
@@ -49,18 +53,17 @@
     # so when it is called, a pandas dataframe
     # is returns containing the execution of
     # the sql query
-    #### YOUR CODE HERE
     def model_data(self, id):
-
-        return f"""
+        query = f"""
             SELECT positive_events, negative_events FROM (
-                    SELECT employee_id
-                         , SUM(positive_events) positive_events
-                         , SUM(negative_events) negative_events
-                    FROM {self.name}
-                    JOIN employee_events
-                        USING({self.name}_id)
-                    WHERE {self.name}.{self.name}_id = {id}
-                    GROUP BY employee_id
-                   )
-                """
+                SELECT employee_id
+                     , SUM(positive_events) positive_events
+                     , SUM(negative_events) negative_events
+                FROM {self.name}
+                JOIN employee_events
+                    USING({self.name}_id)
+                WHERE {self.name}.{self.name}_id = {id}
+                GROUP BY employee_id
+            )
+        """
+        return execute_sql_df(query)
